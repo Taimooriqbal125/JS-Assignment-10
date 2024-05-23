84ddee736cd4fabbd3ffe3f8afbe77b86ed76f79
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-05-23 03:14:13 -0700

    Assignment 10

---
+++ script.js
@@ -0,0 +1,15 @@
+// Write a JS program to find whether a number is negative,positive,0 ?
+
+var number = 10;
+if (number > 0){
+    console.log("Number is positive")
+}
+else if (number < 0){
+    console.log("Number is negative")
+}
+else if (number == 0){
+    console.log("Number is zero")
+}
+else{
+    console.log("noresult")
+}
