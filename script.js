69c134d8c67ac01e6a385d52c74bef9729d6eb19
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-01 02:18:29 -0700

    Problem Solving

--- script.js
+++ script.js
@@ -26,3 +26,26 @@ else if ( characters != 'a' || characters != 'e' || characters != 'i' || charact
         console.log("Character  is not vowal ")
     }
     
+
+    // Write a Js program to input any alphabet and check whether it is alphabet , digit or special character ?
+    
+    var char = 11 ;
+    if (char <= "z"){
+        console.log("Alphabet")
+    }
+    else if(char >= 0 || char >= 9){
+        console.log("Digit")
+    }    
+    else{
+        console.log("Special ")
+    }
+
+    // Write a JS program to check whether a character is uppercase or lowercase alphabet?
+
+    var alph = "A";
+    if(alph <= "Z"){
+        console.log("Upper case")
+    }
+    else if(alph <= "z"){
+        console.log("LowerCase")
+    }
